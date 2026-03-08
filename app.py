6bb1d437a24c5c8276c5f4395bd46b37da9f0e86
FranciscoLoli <94808743+FranciscoLoli@users.noreply.github.com>
2026-03-08 18:17:53 -0500

    add model

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
 import streamlit as st
 import joblib
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
-from nltk.tokenize import RegexpTokenizer
 
-spanish_stopwords = set(stopwords.words('spanish'))
-stemmer = SnowballStemmer("spanish")
-tokenizer = RegexpTokenizer(r'\w+')
-
-def text_preprocess(text):
-    
-    tokens = tokenizer.tokenize(text.lower())
-    tokens = [word for word in tokens if word not in spanish_stopwords]
-    tokens = [stemmer.stem(word) for word in tokens]
-    return ' '.join(tokens)
 
 model = joblib.load("modelo_ods.pkl")
 
---
+++ preprocess.py
@@ -0,0 +1,14 @@
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from nltk.tokenize import RegexpTokenizer
+
+spanish_stopwords = set(stopwords.words('spanish'))
+stemmer = SnowballStemmer("spanish")
+tokenizer = RegexpTokenizer(r'\w+')
+
+def text_preprocess(text):
+    
+    tokens = tokenizer.tokenize(text.lower())
+    tokens = [word for word in tokens if word not in spanish_stopwords]
+    tokens = [stemmer.stem(word) for word in tokens]
+    return ' '.join(tokens)
